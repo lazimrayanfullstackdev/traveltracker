82f895cbfa1a95a4a33349a1e4ab3e35fe338303
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2023-12-07 16:17:39 +0600

    Travel Tracker web app final version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { query } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import ejs from "ejs";
@@ -18,44 +18,50 @@ const db = new pg.Client({
 
 let Countries = [];
 
-db.connect();
-db.query("SELECT * FROM visited_countries", async(err, res)=>{
-if(err){
-  console.log("Error Fetching Data");
-}else{
-  Countries = res.rows.map(row=>row.country_code);
-  console.log("Countries are: "+ Countries);
-  totalCountry = Countries.length;
+// Function to fetch countries from the database
+const fetchCountries = async () => {
+  try {
+    const result = await db.query("SELECT * FROM visited_countries");
+    Countries = result.rows.map(row => row.country_code);
+    totalCountry = Countries.length;
+  } catch (err) {
+    console.log("Error Fetching Data:", err);
   }
-  // await db.end();
-});
+};
 
+db.connect();
+fetchCountries(); // Fetch countries on startup
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   res.render("index.ejs", {
-    countries : Countries,
-    total : totalCountry
-  })
+    countries: Countries,
+    total: totalCountry
+  });
 });
 
 app.post("/add", async (req, res) => {
   const userCountry1 = req.body.country.trim();
   const userCountry = userCountry1.charAt(0).toUpperCase() + userCountry1.slice(1);
-  console.log(userCountry);
 
   try {
     const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [userCountry]);
-    // Check if there are rows returned
+
     if (result.rows.length !== 0) {
       const data = result.rows[0];
       const countryCode = data.country_code;
-      console.log("Match Found");
-    res.redirect("/");
-    }else{
+
+      await db.query("INSERT INTO visited_countries(country_code) VALUES ($1)", [countryCode]);
+
+      // After insertion, fetch the updated data
+      await fetchCountries();
+
+      res.redirect("/");
+    } else {
       console.log("Country Not Matched");
+      res.redirect("/");
     }
   } catch (err) {
     console.error("Error executing query:", err);
